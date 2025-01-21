4890276b5176c1124faffdc26673eda30fe95f77
Simard-Desmeules <2267339@bdeb.qc.ca>
2025-01-21 13:56:59 -0500

    holla

--- Exercices/Exercice2/Exercice2.js
+++ Exercices/Exercice2/Exercice2.js
@@ -1,9 +1,14 @@
 const comboboxChoix = document.getElementById("combobox-choix");
 const boutonAppuyer = document.getElementById("bouton-appuyer");
+const faitesVotreChoix = document.getElementById("faites-votre-choix");
+const messageAvertissement = document.getElementById("message-avertissement");
+var choixActuel = comboboxChoix.options[comboboxChoix.selectedIndex];
+
 boutonAppuyer.addEventListener("click", ()=>{
     var choixActuel = comboboxChoix.value;
-    if(choixActuel == "--SVP Faites votre choix--")
+    if(choixActuel == "faites-votre-choix")
     {
-        
+        window.alert(choixActuel.innerText);
+        messageAvertissement.innerText = "SVP choisir un poste dans la liste ici-haut."
     }
 });
