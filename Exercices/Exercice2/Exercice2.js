a3b0dfc56362ee8da73199baa470465bdebc9acd
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-01-20 22:26:55 -0500

    modifications

---
+++ Exercices/Exercice2/Exercice2.js
@@ -0,0 +1,9 @@
+const comboboxChoix = document.getElementById("combobox-choix");
+const boutonAppuyer = document.getElementById("bouton-appuyer");
+boutonAppuyer.addEventListener("click", ()=>{
+    var choixActuel = comboboxChoix.value;
+    if(choixActuel == "--SVP Faites votre choix--")
+    {
+        
+    }
+});
