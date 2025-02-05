b2914f6a18d73d96b7c3c8d07be3986a2e0c8d12
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-02-05 11:50:16 -0500

    modif

--- Exercices/Projet_etudiant/serveur.js
+++ Exercices/Projet_etudiant/serveur.js
@@ -12,6 +12,8 @@ import dateFormat from "dateformat";
 const app = express();
 const _filename = fileURLToPath(import.meta.url);
 const _dirname = path.dirname(_filename);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /*
 Connexion au serveur
@@ -21,6 +23,7 @@ Connexion au serveur
 const serveur = app.listen(4000, () =>{
     console.log("serveur fonctionne sur 4000... !");
 })
+
 /*
     Configuration de EJS
 */
@@ -50,14 +53,13 @@ con.connect(function(err){
     console.log("connected!");
 }
 
-)
+);
 
 /*
     Description des routes 
  */
 app.get("/", (req, res) => {
- con.query("SELECT * FROM e_events ORDER BY e_start_date DESC",
-    (err,result) => {
+ con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", (err,result) => {
     if(err) throw err;
     res.render("pages/index.ejs", {
         siteTitle: "Application simple",
@@ -67,6 +69,23 @@ app.get("/", (req, res) => {
     });
  });
 });
+
+app.get("/event/edit/:id", function (req, res) {
+  const requete = "SELECT * FROM e_events WHERE e_id = ?";
+  const parametres = [req.params.id];
+  con.query(requete, parametres, function (err, result) {
+    if (err) throw err;
+    result[0].e_start_date = dateFormat(result[0].e_start_date, "yyyy-mm-dd");
+    result[0].e_start_end = dateFormat(result[0].e_end_date, "yyyy-mm-dd");
+    res.render("pages/edit-event.ejs", {
+      siteTitle: "Application simple",
+      pageTitle: "Editer événement : " + result[0].e_name,
+      items: result,
+    });
+  });
+});
+
+
 app.post("/event/edit/:id", function(req,res){
     const requete ="UPDATE e_events SET e_name = ?, e_start_date = ?, e_start_end = ?,e_desc =?, e_location = ? WHERE e_id = ?";
     const parametres = [
@@ -82,6 +101,7 @@ app.post("/event/edit/:id", function(req,res){
         res.redirect("/");
     });
 });
+
 app.get("/event/delete/:id", function(req,res){
     const requete = "DELETE FROM e_event WHERE e_id = ?";
     con.query(requete,[req.params.id], function(err, result){
